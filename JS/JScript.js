6acec9279f25e32fae3550a71e958fd2ac10efdb
Valeriy <tymohovcev@gmail.com>
2022-04-28 12:38:13 +0300

    Second day afternoon

--- JS/JScript.js
+++ JS/JScript.js
@@ -1,61 +1,14 @@
-// применён строгий режим
-"use strict"
-
-// Комментарий на одну строку Ctrl+/
-alert('Hello');
-
-/*Комментарий 
-на несколько срок */
-[1, 2].forEach(alert);
-
-/* Чтоб закаментить часть кода горячими клавишами Shift+Alt+A */
-
-/* 
-
-let message;
-message = "сообщение в присвоенное переменной";
-alert (message); 
-
-*/
-
-// Короткая записать присвоения переменной
-let message = 'Bye',
-    user = 'John',
-    age = '31';
-alert(user);
-alert(age);
-alert(message);
-
-// Применяется только последнее присвоеное значение переменной, последнее перебивает все предыдущие
-
-let test;
-test = 'chicken';
-test = 'rabbit';
-alert(test);
-
-// Строка с обратныеми ковычками
-
-let name = "Валерчик";
-alert(`Добрый день, ${name}!`);
-alert(`Добрый день, число ${3+5}!`);
-
-// Булевый(логический) тип данных true/false
-
-let isGreater = 4 > 3;
-alert(isGreater);
-
-// Функция prompt
-
-let howOld = prompt('Сколько Вам годиков?');
-alert(`Получается, что тебе ${howOld} лет`);
-
-// Функция comfirm
-
-let question = confirm('Правда, что у тебя есть грыжа L5?');
-alert(`Так как ты отвеил - ${question}. Значит это место и будем лечить`);
-
-
 // Преобразование типов
+// Из строки в boolean
 
-let value = true;
-alert(typeof value);
+let match = "хуй";
+alert(typeof match);
+
+match = String("Украина");
+alert(typeof match);
+
+// из строки в Number
+let ext = "привет";
+
+tex = Boolean(ext);
+alert(typeof tex);
